5974c2d2289ad6206e73f1fcc2bf11c1c58baaf3
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-08-22 19:54:06 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 이 부분을 수정합니다.
+  base: "/tets_ai/", // 이 부분을 수정합니다.
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
